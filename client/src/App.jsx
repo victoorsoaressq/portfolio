f575d8e94a9f216510cf40de470f171cf14e1a96
victoorsoaressq <victoorsoaressq@gmail.com>
2024-08-15 10:36:19 -0300

    Build
    New project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,12 +10,14 @@ import IMG1 from '/img/projeto1.png'
 import IMG2 from '/img/iosSantander.png'
 import IMG3 from '/img/projeto3.png'
 import IMG4 from '/img/iosDuolingo.png'
+import IMG5 from '/img/projeto4.png'
 
 import ICON1 from '/icons/css3.svg'
 import ICON2 from '/icons/javascript.svg'
 import ICON4 from '/icons/react.svg'
 import ICON5 from '/icons/swift2.png'
 import ICON6 from '/icons/swiftui.png'
+import ICON7 from '/icons/html5.svg'
 
 function App() {
   return (
@@ -30,7 +32,7 @@ function App() {
             <h2>Quem sou eu</h2>
 
             <p>
-              Olá, meu nome é Victor e sou <span> Desenvolvedor IOS</span>,
+              Olá, meu nome é Victor e sou <span> Desenvolvedor Front-end & IOS</span>,
             </p>
             <p>
               gosto de ver o mundo através de linhas de códigos, com bastante estudo e criatividade crio e copio APPs em busca de me tornar um especialista em IOS.
@@ -87,6 +89,36 @@ function App() {
       </section>
 
       <section id='projetos' >
+        <Section_title
+          title='projeto destaque'
+          descriptive='Meu maior projeto como desenvolvedor Front-end'
+        />
+
+        <div className='section_content bottom'>
+          <div className='project' id='reverse'>
+            <img src={IMG5} alt="notebook" />
+            <div className='section_c_descriptive' id='project_descriptive_center'>
+              <div className='project_icons'>
+                <img src={ICON7} alt="Html" />
+                <img src={ICON1} alt="Css" />
+                <img src={ICON2} alt="Js" />
+                <img src={ICON4} alt="React" />
+              </div>
+              <h2>Front-end Map</h2>
+              <p>
+                Site desenvolvido para ensinar as principais tecnologias <span>front-end</span> de forma totalmente gratuita.
+              </p>
+              <div className='buttons_project'>
+                <a href="https://www.frontendmap.com/" target='_blank'>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z" /></svg>
+                  Site
+                </a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
         <Section_title
           title='projetos'
           descriptive='Aqui você encontrará alguns dos projetos pessoais e de clientes que criei, cada projeto contendo seu próprio estudo de caso'
@@ -176,7 +208,7 @@ function App() {
             </div>
           </div>
 
-          <div className='project'  id='reverse'>
+          <div className='project' id='reverse'>
             <img src={IMG3} alt="notebook" />
             <div className='section_c_descriptive' id='project_descriptive_center'>
               <div className='project_icons'>
